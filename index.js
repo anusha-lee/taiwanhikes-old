283b98515ef6ce199fe0ac01eb33a39a90b8c3bf
Anusha Lee <anushalee@gmx.com>
2019-08-24 22:30:46 +0800

    style 7 reasons page layout for bigger screen

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-console.log('Hello World from Taiwan')
-console.log('Test 123')
-
 import './sass/main.scss'
+
+let navFixed = document.querySelectorAll('header');
+
+window.addEventListener("scroll", function(e) {
+    //console.log("you scroll");
+    let fromTop = window.scrollY;
+    if (fromTop > 500) {
+        console.log(fromTop, "You scroll down");
+    } else if (fromTop < 0) {
+        console.log("You scroll up");
+    }
+    
+})
+
