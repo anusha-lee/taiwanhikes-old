01b9e39aeb6f2c158d01c016e13aa368aba92ddc
Anusha Lee <anushalee@gmx.com>
2019-09-27 23:44:45 +0800

    found Date.parse() to convert string into numbers, but need to loop through the data with date to sort the order

--- index.js
+++ index.js
@@ -79,10 +79,10 @@ form.addEventListener('submit', function (e) {
 Trips api
 *********************************************/
 
-let url2trips = "https://api.myjson.com/bins/r6k4l";
+let url = "https://api.myjson.com/bins/yt8bl";
 let newHTML2 = document.getElementById("oneDayFree");
 
-fetch(url2trips)
+fetch(url)
     .then(res3 => res3.json())
     .then(arr => {       
         
@@ -96,16 +96,16 @@ fetch(url2trips)
             // Create and append the li's to the ul or divs to the parent div
              newHTML2.innerHTML += `
                 <div class="events__oneDayFree--trip freeTrip">
-                    <h3 id="freeTrip1">活動Event:${val.活動Event}</h3>
-                    <div class="freeTrip__date"><strong>日期Date:</strong> ${val.日期Date}</div>
-                    <div class="freeTrip__meetup-time"><strong>集合時間Meetup Time:</strong> ${val.集合時間Meetup_Time}</div>
-                    <div class="freeTrip__meetup-point"><strong>集合地點Meetup Point:</strong> ${val.集合地點Meetup_Point}</div>
-                    <div class="freeTrip__transport"><strong>交通Transportation:</strong> ${val.交通Transportation}</div>
-                    <div class="freeTrip__distance"><strong>里程Distance:</strong> ${val.里程Distance}</div>
-                    <div class="freeTrip__difficulty-level"><strong>難度Difficulty Level:</strong> ${val.難度Difficulty_Level}</div>
-                    <div class="freeTrip__club"><a href="#"><strong>社團Hiking Club:</strong> <a href="#">${val.社團Hiking_Club}</a></div>
-                    <div class="freeTrip__guide"><strong>領隊Guide:</strong> ${val.響導Guide}</div>
-                    <div class="freeTrip__about-Trail"><strong>路線簡介About the trail: <br></strong> ${val.路線簡介About_the_trail}</div>
+                    <h3>活動Event:${val.Event}</h3>
+                    <div class="freeTrip__date"><strong>日期Date:</strong> ${val.Date}</div>
+                    <div class="freeTrip__meetup-time"><strong>集合時間Meetup Time:</strong> ${val.Meetup_Time}</div>
+                    <div class="freeTrip__meetup-point"><strong>集合地點Meetup Point:</strong> ${val.Meetup_Point}</div>
+                    <div class="freeTrip__transport"><strong>交通Transportation:</strong> ${val.Transportation}</div>
+                    <div class="freeTrip__distance"><strong>里程Distance:</strong> ${val.Distance}</div>
+                    <div class="freeTrip__difficulty-level"><strong>難度Difficulty Level:</strong> ${val.Difficulty_Level}</div>
+                    <div class="freeTrip__club"><a href="#"><strong>社團Hiking Club:</strong> <a href="#">${val.Hiking_Club}</a></div>
+                    <div class="freeTrip__guide"><strong>領隊Guide:</strong> ${val.Guide}</div>
+                    <div class="freeTrip__about-Trail"><strong>路線簡介About the trail: <br></strong> ${val.About_the_trail}</div>
             `;
            }
         }
